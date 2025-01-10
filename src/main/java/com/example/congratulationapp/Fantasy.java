0f8879731745953757515439f67f70a10d20feae
dudon <dudomkamalush@gmail.com>
2025-01-10 22:12:52 +0200

    add Fantasy

---
+++ src/main/java/com/example/congratulationapp/Fantasy.java
@@ -0,0 +1,12 @@
+package com.example.congratulationapp;
+
+public class Fantasy {
+    private final String result;
+    public Fantasy(String Gender, String Name, String Species, String Genre) {
+        String temp ="";
+        result = temp;
+    }
+    public String FantasyText(){
+        return result;
+    }
+}
